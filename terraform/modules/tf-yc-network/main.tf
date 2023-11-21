3af2aba834bc918ffad446c9194518e2dc63128a
skayloo <skayloo@yandex.ru>
2023-11-22 02:03:11 +0300

    Fixing some shit

--- terraform/modules/tf-yc-network/main.tf
+++ terraform/modules/tf-yc-network/main.tf
@@ -2,7 +2,7 @@ data "yandex_vpc_network" "default" {
   name = "default"
 }
 
-data "yandex_vpc_subnet" "default"{
+data "yandex_vpc_subnet" "default" {
     for_each = toset(var.subnet_list)
     name = "${data.yandex_vpc_network.default.name}-${each.key}"
 }
