45238299d9e6d6ae3b01b00b2bb660eefe3f8262
skayloo <skayloo@yandex.ru>
2023-11-22 02:06:09 +0300

    Fixing some more shit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,5 +2,4 @@ provider "yandex" {
   cloud_id                 = var.cloud_id_var
   folder_id                = var.folder_id_var
   zone                     = var.zone_var
-  service_account_key_file = var.key_file_path
 }
